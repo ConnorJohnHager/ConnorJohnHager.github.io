1bef0aeaf678cf0f91c52d97e1b981f8d9ed3474
Connor Hager <connorhager@connors-mbp.mynetworksettings.com>
2023-12-20 16:28:55 -0500

    Setting up Interest Form -12/20

--- script.js
+++ script.js
@@ -5,13 +5,30 @@
 
 // Hamburger menu function
 function hamburger() {
-    var navlinks = document.getElementById("nav-links");
-    var menuicon = document.getElementById("icon");
+    let navlinks = document.getElementById("nav-links");
+    let menuicon = document.getElementById("icon");
     if (navlinks.style.display === "block") {
         navlinks.style.display = "none";
         menuicon.style.color = "#00A5CF"; 
     } else {
         navlinks.style.display = "block";
         menuicon.style.color = "#00A5CF"; 
+    }
+}
+
+// Interest form function
+function interestForm() {
+    document.getElementById("interestForm").addEventListener("submit", function(event) {
+        let emailInput = document.getElementById("email");
+        
+        if (!isValidEmail(emailInput.value)) {
+            alert("Please enter a valid email address.");
+            event.preventDefault();
+        }
+    });
+
+    function isValidEmail(email) {
+        let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
     }
 }
