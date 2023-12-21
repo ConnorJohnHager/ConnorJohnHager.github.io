23f01809253faf33c507bf24f998f87e8b8388aa
Connor Hager <connorhager@connors-mbp.mynetworksettings.com>
2023-12-20 20:04:20 -0500

    Removed form since PHP not supported, updated spelling -12/20

--- script.js
+++ script.js
@@ -15,20 +15,3 @@ function hamburger() {
         menuicon.style.color = "#00A5CF"; 
     }
 }
-
-// Interest form function
-function interestForm() {
-    document.getElementById("interestForm").addEventListener("submit", function(event) {
-        let emailInput = document.getElementById("email");
-        
-        if (!isValidEmail(emailInput.value)) {
-            alert("Please enter a valid email address.");
-            event.preventDefault();
-        }
-    });
-
-    function isValidEmail(email) {
-        let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
-}
